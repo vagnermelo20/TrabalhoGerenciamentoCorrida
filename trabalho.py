c508e1d76e14e935d3185717871397dfc0e805ee
vagnermelo20 <vagnermontenegro20@gmail.com>
2024-10-30 23:52:33 -0300

    ajustes no código de criar_dados, eliminando código da funcao salvar_no_banco que foi colocado, tambem, na funcao criar_dados. Além de ajeitar o que havia salvo no banco de dados

--- trabalho.py
+++ trabalho.py
@@ -3,9 +3,6 @@ def criar_dados(treinoOUcompeticao, data, distancia, tempo, localizacao, condica
         return f"treino: data: {data}, distância percorrida: {distancia} km, tempo: {tempo} min, localização: {localizacao}, condições climáticas: {condicaoClimatica}\n"
     elif treinoOUcompeticao == 'competicao':
         return f"competição: data: {data}, distância percorrida: {distancia}, tempo: {tempo}, localização: {localizacao}, condições climáticas: {condicaoClimatica}\n"
-    entrada = criar_dados(treinoOUcompeticao, data, distancia, tempo, localizacao, condicaoClimatica)
-    with open(arquivo_nome, "a") as arquivo:
-        arquivo.write(entrada)
 def salvar_no_banco(treinoOUcompeticao, data, distancia, tempo, localizacao, condicaoClimatica, arquivo_nome="banco.txt"):
     entrada = criar_dados(treinoOUcompeticao, data, distancia, tempo, localizacao, condicaoClimatica)
     with open(arquivo_nome, "a", encoding="utf-8") as arquivo:
