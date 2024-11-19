3a2c5961ab1e2b095a175b17c2cea541cddfb00c
vagnermelo20 <vagnermontenegro20@gmail.com>
2024-11-19 16:11:58 -0300

    Revert "Merge branch 'main' of https://github.com/vagnermelo20/TrabalhoGerenciamentoCorrida"
    This reverts commit efd2c636b8cd40e7fc4095225e316be7ed8ef55c, reversing
    changes made to 1bce609134ef16fff2e221f6a42cae2d5cf2cdb1.

--- ler_linhas_individuais.py
+++
@@ -1,7 +0,0 @@
-def ler_linhas_individuais(arquivo_nome="banco.txt"):
-
-    with open(arquivo_nome, "r", encoding="utf-8") as arquivo:
-        texto = arquivo.readlines()
-        for linha in texto:
-            certo = print(linha, end="")
-            return certo
